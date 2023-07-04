e47ecec54d4e3d026a64af5d4ac0aec9cd0d90f4
Farid0558475484 <faridmh@code.edu.az>
2023-07-04 19:27:43 +0400

    feat:start

---
+++ CollegeApp/Controllers/StudentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using CollegeApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Student> GetStudents()
+        {
+            return new List<Student>
+            {
+                new Student
+                {
+                    Id = 1,
+                    StudentName = "John",
+                    Email = "john@example.com",
+                    Address = "123 Main St"
+                },
+                new Student
+                {
+                    Id = 2,
+                    StudentName = "Mary",
+                    Email = "mary@example.com",
+                    Address = "456 Elm St"
+                }
+                
+            };
+        }
+    }
+}
